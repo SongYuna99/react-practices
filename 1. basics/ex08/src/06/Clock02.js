ae0741de069dcbffba4b460568fc1b01d4a10260
SongYuna99 <thddbsk7248@gmail.com>
2023-10-31 15:53:11 +0900

    Component #1

--- 1. basics/ex08/src/06/Clock02.js
+++ 1. basics/ex08/src/06/Clock02.js
@@ -7,25 +7,31 @@ function Clock02(props) {
   const seconds = now.getSeconds();
   let session = "AM";
 
-  if (hours > 12) {
-    session = "PM";
-  }
-
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
   return (
-    <div>
-      {hours}:{minutes}:{seconds} {session}
+    /*
+      Comment01: JSX밖은 JavaScript 구문(주석 구문)이 가능하다.
+    */
+    <div
+      /*
+        Comment02: JSX Element 태그 안에서도 주석은 가능하다.(비추)
+      */
+      className="clock"
+      title="시계"
+    >
+      {/*
+          Comment03: 이런 방식으로 표현식이 실행되는 블록 안에서 주석을 단다.(개추)
+        */}
+      {("0" + (hours > 12 ? hours - 12 : hours)).slice(-2)}
+      {":"}
+      {/*
+          Comment04: JSX는 HTML이 아니다!
+                     따라서 <!-- --> 주석은 사용할 수 없다.
+        */}
+      {("0" + minutes).slice(-2)}
+      {":"}
+      // Comment05: JSX안에서 JavaScript 주석 구문(/* */을 포함)을 사용하면
+      화면에 그대로 나온다.
+      {("0" + seconds).slice(-2)} {hours > 12 ? "PM" : "AM"}
     </div>
   );
 }
