a908c8a6c9e3d0654cd4b756bf1f1791a567ebe5
SongYuna99 <thddbsk7248@gmail.com>
2023-11-01 16:26:09 +0900

    emaillist #3

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import RegisterForm from "./RegisterForm";
+import SearchBar from "./SearchBar";
+import Emaillist from "./Emaillist";
+import data from "./assets/json/data.js";
+import "./assets/scss/App.scss";
 
 function App() {
-  return <div id="{ 'App' }">Emaillist</div>;
+  return (
+    <div id={"App"}>
+      <RegisterForm />
+      <SearchBar />
+      <Emaillist emails={data} />
+    </div>
+  );
 }
 
 export { App };
